dc804e20ea4145c72ee991dc3afac3ca6e2fc502
nhando <trongnhan19081994@gmail.com>
2021-03-18 10:42:28 +0700

    update class function component

---
+++ helloword/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Greet from './components/greet';
+import Welcome from './components/welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ helloword/src/components/greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Greet(){
+//     return <h1> Hello function component</h1>
+// }
+
+const Greet = () => <h1> Hello arrow function component</h1>
+
+export default Greet;
