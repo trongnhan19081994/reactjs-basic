a5c84a011b52cc235e173c60ac43a5d4b59d29d4
Nhan Do <trongnhan19081994@gmail.com>
2021-03-22 22:04:52 +0700

    higher order component

--- higher-order-components/src/App.js
+++ higher-order-components/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ClickCounter from './component/ClickCounter';
+import HoverCounter from './component/HoverCounter';
 
 function App() {
   return (
     <div className="App">
-
+      <ClickCounter name="TrongNhan" />
+      <HoverCounter />
     </div>
   );
 }
---
+++ higher-order-components/src/component/ClickCounter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import withCounter from './withCounter';
+
+class ClickCounter extends Component {
+
+    render() {
+        const { count, incrementCount, name } = this.props;
+        return (
+            <div>
+                <button onClick={incrementCount}> {name} Clicked {count} Times</button>
+            </div>
+        )
+    }
+}
+
+export default withCounter(ClickCounter, 5);
