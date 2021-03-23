05e734fc3ad0c62d5ebecbfa26220dca65944e57
Nhan Do <trongnhan19081994@gmail.com>
2021-03-23 23:11:59 +0700

    state hook

---
+++ state-hook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      <HookCounter />
+    </div>
+  );
+}
+
+export default App;
