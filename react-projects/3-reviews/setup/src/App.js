1c94b8cff0360160348a68a01ed70604d788606f
Nhan Do <trongnhan19081994@gmail.com>
2021-04-06 22:09:54 +0700

    update react-projects

---
+++ react-projects/3-reviews/setup/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return <main>
+    <div className='container'>
+      <div className='title'>
+        <h2> Out Reviews </h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+    </div>
+  </main>;
+}
+
+export default App;
