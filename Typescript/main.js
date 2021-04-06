12d5d2259c1b7d900ec7204c2bea045494dcc837
Nhan Do <trongnhan19081994@gmail.com>
2021-04-06 20:45:18 +0700

    update

---
+++ Typescript/main.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Hello word';
+console.log(message);
+var isBeginner = true;
+var total = 0;
+var name = 'Vishwas';
+var sentence = "My name is " + name + " \n    I'm a beginner in Typescript\n";
+console.log(sentence);
---
+++ Typescript/main.ts
@@ -0,0 +1,13 @@
+export {}
+
+let message = 'Hello word';
+console.log(message);
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Vishwas';
+
+let sentence: string = `My name is ${name} 
+    I'm a beginner in Typescript
+`;
+console.log(sentence);
